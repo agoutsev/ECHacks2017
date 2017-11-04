baac8cdc171e57d02896e3f4c600ac73fb210ece
agoutsev <agoutsev@uwaterloo.ca>
2017-11-04 19:29:20 -0400

    update
    update

--- CSVParser.cs
+++ CSVParser.cs
@@ -27,8 +27,6 @@ namespace CSVParser
             {
                 web.DownloadFile(csvQueryData, filePathData);
                 web.DownloadFile(csvQueryMetaData, filePathMetaData);
-                Console.WriteLine(filePathData + " : download success");
-                Console.WriteLine(filePathMetaData + " : download success");
                 data = new FileInfo(filePathData);
                 metadata = new FileInfo(filePathMetaData);
             }
@@ -42,8 +40,8 @@ namespace CSVParser
         public void OpenStockData(string company, string start_date, string end_date)
         {
             Close();
-            string filePathData = String.Format(@"C:\Users\JoltA\Documents\Visual Studio 2017\Projects\csvtest\{0}-data.csv", company);
-            string filePathMetaData = String.Format(@"C:\Users\JoltA\Documents\Visual Studio 2017\Projects\csvtest\{0}-metadata.csv", company);
+            string filePathData = String.Format(@"..\{0}-data.csv", company);
+            string filePathMetaData = String.Format(@"..\{0}-metadata.csv", company);
             string csvQueryData = String.Format("https://www.quandl.com/api/v3/datasets/WIKI/{0}/data.csv?start_date={1}&end_date={2}&api_key=-cgVuGXX5HzSwHStGPPc", company, start_date, end_date);
             string csvQueryMetaData = String.Format("https://www.quandl.com/api/v3/datasets/WIKI/{0}/metadata.csv?&start_date={1}&end_date={2}&api_key=-cgVuGXX5HzSwHStGPPc", company, start_date, end_date);
 
@@ -54,8 +52,6 @@ namespace CSVParser
 
                 web.DownloadFile(csvQueryData, filePathData);
                 web.DownloadFile(csvQueryMetaData, filePathMetaData);
-                Console.WriteLine(filePathData + " : download success");
-                Console.WriteLine(filePathMetaData + " : download success");
                 data = new FileInfo(filePathData);
                 metadata = new FileInfo(filePathMetaData);
             }
